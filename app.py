d9b8123bcc3277f6a933c474d329ecede7164b0d
Sajid16 <sajid.mahboob16@gmail.com>
2020-03-12 21:29:42 +0600

    full crud done and practice to 1:53:42sec

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -25,12 +25,46 @@ def index():
 
 @app.route('/posts/', methods=['GET', 'POST'])
 def posts():
-    return render_template('posts.html')
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        content = request.form['content']
+        # print(title)
+        # print(author)
+        # print(content)
+
+        new_post = BlogPost(title=title, content=content, author=author)
+        # print('on line 34')
+        db.session.add(new_post)
+        # print('on line 36')
+        db.session.commit()
+        # print('on line 38')
+        return redirect(url_for('posts'))
+    else:
+        all_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
+        return render_template('posts.html', posts=all_posts)
 
 
-@app.route('/new_posts/', methods=['GET', 'POST'])
-def new_posts():
-    return render_template('new_post.html')
+@app.route('/posts/delete/<int:post_id>')
+def delete(post_id):
+    delete_post = BlogPost.query.get_or_404(post_id)
+    db.session.delete(delete_post)
+    db.session.commit()
+    return redirect(url_for('posts'))
+
+
+@app.route('/posts/update/<int:post_id>')
+def update(post_id):
+    edit_post = BlogPost.query.get_or_404(post_id)
+
+    if request.method == 'POST':
+        edit_post.title = request.form['title']
+        edit_post.author = request.form['author']
+        edit_post.content = request.form['content']
+        db.session.commit()
+        return redirect(url_for('posts'))
+    else:
+        return render_template('edit.html', post=edit_post)
 
 
 if __name__ == "__main__":
